eb331e7bb545d83c124606f9c773fd0010171f13
sunset1117 <sunset1117@hotmail.com>
2020-06-04 01:46:15 +0800

    6/4 01:45購物車頁面Checkbox完成連動能即時更新待結帳的消息 要再繼續完成刪除功能。

--- product/js/shoppingCar.js
+++ product/js/shoppingCar.js
@@ -4,10 +4,11 @@ $(window).on("load", function(){
 
 $(document).on("change", function(){
   subtotal();
-  console.log($(".selectOne").attr("checked"));
+  prepareCheckout();
+  reverseCheckbox();
 });
 
-//計算小計
+//單一商品小計
 function subtotal(){
   for(let i=0; i<$(".subtotal").length; i++){
     let price = $(".price")[i].innerHTML;
@@ -16,39 +17,53 @@ function subtotal(){
   }
 }
 
+
+
+// 選中的商品結算
+function prepareCheckout(){
+  let price = 0;
+  let count = 0;
+  $(".sumPrice").html(0);
+  $(".sumQuantity").html(0);
+  $(".selectOne").each(function(){
+    if(this.checked==true){
+      price += parseInt($(this).closest("tr").find(".subtotal").html());
+      count += parseInt($(this).closest("tr").find(".input_quantity").val());
+      $(".sumPrice").html(price);
+      $(".sumQuantity").html(count);
+      // console.log(count);
+      // console.log(price);
+    }
+  });
+}
+
+
+
+
+
+
+
+// checkbox全選
 $(".selectAll").on("click", function(){
-  if($(this)[0].checked){
-    $(".selectOne").attr("checked", "checked");
-  }else{
-    $(".selectOne").removeAttr("checked");
+  for(let i=0; i<$(".selectOne").length; i++){
+    $(".selectOne")[i].checked = $(this)[0].checked;
   }
 });
 
-$(".selectOne").on("click", function(){
-  $(".selectOne").closest("td").children("input").toggleClass("-on");
-  // $(".selectOne").toggleClass("-on");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// reverseCheckbox
+function reverseCheckbox(){
+  let count = 0;
+  $(".selectOne").each(function(){
+    if(this.checked==true){
+      count++
+    }
+  });
+  if($(".selectOne").length == count){
+    $(".selectAll").prop("checked", true);
+  }else {
+    $(".selectAll").prop("checked", false);
+  }
+}
 
 
 
